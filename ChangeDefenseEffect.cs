bf976a0040d0ebfd3c0e55deee80a2f0d7538f2d
Alexander Mendelsohn <superparkourio@gmail.com>
2016-03-04 15:48:56 -0500

    Commit
    These are all the scripts and components I've worked on so far.

---
+++ ChangeDefenseEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeDefenseEffect : Effect {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.time -= Time.deltaTime;
+		if (this.time <= 0) {
+			reverseEffect ();
+		} else if (!didIt) {
+			doEffect (); // doEffect() is here because this effect must modify a value at regular intervals, whereas halving armor is just once
+		}
+	}
+
+	public override void doEffect() {
+		//do whatever
+		didIt = true;
+
+	}
+
+	public override void reverseEffect() {
+		//stub
+	}
+}
